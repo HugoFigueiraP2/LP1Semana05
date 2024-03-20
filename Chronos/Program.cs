edec38f5c41ca4ccd0e5dc32483448f68a2ac218
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-20 15:25:20 +0000

    Adding Start, Pause and Stop methods

---
+++ Chronos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Chronos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono1 = new Stopwatch();
+            Stopwatch crono2 = new Stopwatch();
+
+            crono1.Start();
+            System.Threading.Thread.Sleep(600);
+            crono2.Start();
+            System.Threading.Thread.Sleep(200);
+            crono1.Stop();
+            crono2.Stop();
+
+            Console.WriteLine("Crono 1: " + )
+
+
+
+        }
+    }
+}
