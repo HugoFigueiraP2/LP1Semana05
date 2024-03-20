0e24c779caa0c8949a99feb2c994efa556db1a8f
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-20 17:03:45 +0000

    Completing Exercise 10

--- MyGame/Program.cs
+++ MyGame/Program.cs
@@ -6,7 +6,23 @@ namespace MyGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n_inimigos = int.Parse(args[0]);
+
+            Enemy[] enemies  = new Enemy [n_inimigos]; // tamanho da lista
+
+            for(int i = 0; i < n_inimigos; i++)
+            {
+                Console.Write($"Nome do inimigo {i + 1}: ");
+                string nome_inimigo = Console.ReadLine();
+                enemies[i] = new Enemy(nome_inimigo);
+
+            }
+
+            foreach(Enemy j in enemies)
+            {
+                Console.WriteLine($"{j.GetName()} {j.GetHealth()} {j.GetShield()}");
+
+            }
         }
     }
 }
