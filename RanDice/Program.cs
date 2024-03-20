bd68c0e58bcb9f2344177f3449ac92f5a075f3e0
Hugo Silva <hugofigueira.dev@gmail.com>
2024-03-20 15:53:29 +0000

    Completing Exercise 5

---
+++ RanDice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RanDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(args[0]);
+            int s = int.Parse(args[1]);
+
+            Random random_Value = new Random(s);
+
+            int value = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                int value_between = random_Value.Next(1,7);
+                value += value_between;
+                
+            }
+            Console.WriteLine(value);
+        }
+    }
+}
