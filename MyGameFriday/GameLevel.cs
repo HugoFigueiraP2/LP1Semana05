3dc8dd5cda140aed9b75a51f9183aa2d1aa0e068
HugoFigueiraP2 <hugofigueira.dev@gmail.com>
2024-03-22 13:46:25 +0000

    Addind some methods

---
+++ MyGameFriday/GameLevel.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace MyGameFriday
+{
+    public class GameLevel
+    {
+        private int number_rooms;
+        private int [] room;
+        private Difficulty level;
+
+        public GameLevel(int number_rooms, Difficulty level)
+        { 
+            this.number_rooms = room[number_rooms]
+
+        }
+        
+        SetEnemyInRoom(int ind_room, string Enemy)
+        {
+            if(Enemy in room[ind_room])
+            {
+                Console.WriteLine
+                ("Já tem um inimigo a ocupar esta sala!")
+            }
+            else:
+            {
+                
+            }
+
+            
+
+
+        }
+    }
+}
