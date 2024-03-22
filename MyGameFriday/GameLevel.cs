da7704c7eb6f632750f8226f43de0757aada5c12
HugoFigueiraP2 <hugofigueira.dev@gmail.com>
2024-03-22 14:18:39 +0000

    Adding most of the methods

--- MyGameFriday/GameLevel.cs
+++ MyGameFriday/GameLevel.cs
@@ -5,30 +5,71 @@ namespace MyGameFriday
     public class GameLevel
     {
         private int number_rooms;
-        private int [] room;
+        private string [] room;
         private Difficulty level;
+        private int enemy_numbers;
 
         public GameLevel(int number_rooms, Difficulty level)
         { 
-            this.number_rooms = room[number_rooms]
-
+            GetNumRooms(number_rooms);
+            GetDifficulty(level);
+            enemy_numbers = 0;
+            room[number_rooms];
         }
         
-        SetEnemyInRoom(int ind_room, string Enemy)
+        public void SetEnemyInRoom(int ind_room, Enemy enemy)
         {
-            if(Enemy in room[ind_room])
+            if(enemy in room[ind_room])
             {
                 Console.WriteLine
-                ("Já tem um inimigo a ocupar esta sala!")
+                ("Já tem um inimigo a ocupar esta sala!");
             }
-            else:
+            else
             {
-                
+                enemy_numbers ++;
+                room[ind_room] = enemy;
+
             }
-
-            
-
 
         }
+
+        public Difficulty GetDifficulty()
+        {
+            return level;
+
+        }
+        public int GetNumRooms()
+        {
+            return number_rooms;
+
+        }
+
+        public int GetNumEnemies()
+        {
+            return enemy_numbers;
+
+        }
+
+        public string PrintEnemies()
+        {
+             // Este programa mostra o seguinte no ecrã:
+            //
+            for(int i = 0; i < GetNumEnemies; i++)
+            {
+            Console.WriteLine($"Room {room[en]}")
+            }
+            
+            // Difficulty: Easy
+            // Number of rooms: 101
+            // Number of enemies: 5
+            // Room 1: Ringo
+            // Room 2: Tina
+            // Room 18: Faker
+            // Room 57: Out of order
+            // Room 98: Chet
+        }
+
+        
+
     }
 }
